9dc896a622cf230590441a5a7090f58018bc18e2
Alfakynz <alfakynz.nyxaeris@gmail.com>
2026-02-15 20:05:45 +0100

    First version

---
+++ src/main/java/com/alfakynz/ltxec/Ltxec.java
@@ -0,0 +1,16 @@
+package com.alfakynz.ltxec;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ltxec implements ModInitializer {
+	public static final String MOD_ID = "lt-x-ec";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
